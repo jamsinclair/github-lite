eef69f290aa0f680412d6fee415ebbe61d70df0f
jamsinclair <jamsinclairnz@gmail.com>
2016-11-13 21:17:48 +1300

    Add test spec

---
+++ test/api.spec.js
@@ -0,0 +1,32 @@
+'use strict'
+
+import chai from 'chai'
+import chaiAsPromised from 'chai-as-promised'
+chai.use(chaiAsPromised)
+const expect = chai.expect
+
+import BaseGitHubApi from '../src/api'
+
+describe('BaseGitHubApi', function () {
+  it('should return a valid api instance without config', () => {
+    const api = new BaseGitHubApi()
+    expect(api).to.be.an.instanceOf(BaseGitHubApi)
+  })
+
+  describe('#doRequest', function () {
+    describe('without authentication', function () {
+      it('should be able to make requests to public api', () => {
+        const apiEndpoint = '/zen'
+        const api = new BaseGitHubApi()
+
+        const promise = api.doRequest(apiEndpoint)
+
+        return promise.then((res) => {
+          expect(res).to.be.an.object
+          expect(res).to.have.property('status')
+          expect(res.status).to.equal(200)
+        })
+      })
+    })
+  })
+})
