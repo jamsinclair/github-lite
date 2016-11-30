a0651499089a9cb5648b08713dc6f298ea667f94
jamsinclair <jamsinclairnz@gmail.com>
2016-12-01 11:24:39 +1300

    Add branches and issues to the api

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import Client from './client'
+import Branches from './branches'
+import Issues from './issues'
 import PullRequests from './pulls'
 import Repositories from './repos'
 import PageLinks from './paging'
 
 export default function (config) {
   return new Client(config, {
+    Branches,
+    Issues,
     PullRequests,
     Repositories,
     PageLinks
